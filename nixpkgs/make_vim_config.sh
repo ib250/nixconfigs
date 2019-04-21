feeb9eda8a4e6493b746e49ddb22a42349e8c869
Ismail <ismail.bell.513@gmail.com>
2019-04-21 10:05:49 +0100

    latest commit -- before artix migration

--- nixpkgs/make_vim_config.sh
+++ nixpkgs/make_vim_config.sh
@@ -7,39 +7,39 @@ __USAGE__="
 
 make_vim_config() {
 
-status_=1
-rcend="
-source $(pwd)/vimrc_minimal
-\" add new stuff here
-"
+    status_=1
+    rcend="
+    source $(pwd)/vimrc_minimal
+    \" add new stuff here
+    "
 
-mkdir -p ~/.vim/bundle
+    mkdir -p ~/.vim/bundle
 
-[ -d ~/.vim/bundle/Vundle.vim ] || {
+    [ -d ~/.vim/bundle/Vundle.vim ] || {
 
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || {
+        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || {
         status_=0
-        echo "couldn't find vundle and was unable to clone into .vim"
-        exit status_
+                echo "couldn't find vundle and was unable to clone into .vim"
+                exit status_
+            }
+
+    } && echo "Vundle already found..."
+
+
+    [ -e ~/.vimrc ] && echo "An existing vimrc aready found" || {
+
+        cp vimrc_vundle ~/.vimrc || {
+            status_=0
+            echo "unable to create template vimrc"
+            exit status_
+        }
+
+        echo ${rcend} >> ~/.vimrc
+
     }
 
-} && echo "Vundle already found..."
 
-
-[ -e ~/.vimrc ] && echo "An existing vimrc aready found" || {
-
-    cp vimrc_vundle ~/.vimrc || {
-        status_=0
-        echo "unable to create template vimrc"
-        exit status_
-    }
-
-    echo ${rcend} >> ~/.vimrc
-
-}
-
-
-[ ${status_} -eq 1 ] && vim "+:PluginInstall"
+    [ ${status_} -eq 1 ] && vim "+:PluginInstall"
 
 }
 
