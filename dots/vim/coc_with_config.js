6aa549f3df514c6ab663c3f76a2e4dc3f95ad616
Ismail <ismail.bell.513@gmail.com>
2019-11-02 19:59:25 +0000

    moar cleanup

--- dots/vim/coc_with_config.js
+++ dots/vim/coc_with_config.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-const fs = require("fs");
+const fs = require("fs")
 
 
 const whichLang = (binPath, ls) => {
@@ -17,20 +17,23 @@ const whichLang = (binPath, ls) => {
                     ".hg/"
                 ],
                 filetypes: ["c", "cpp", "objc", "objcpp"]
-            };
+            }
         case "metals":
             return { 
                 command: binPath,
                 rootPatterns: ["build.sbt"],
                 filetypes: ["scala", "sbt"]
-            };
+            }
         default:
             throw "unrecognised language server, maybe update dots?"
     }
-};
+}
 
 const includeLanguageServer = (cocSettings, binPath, languageServer) => {
-    const settings = fs.existsSync(cocSettings) ? JSON.parse(fs.readFileSync(cocSettings)) : {};
+    const settings = fs.existsSync(cocSettings)
+        ? JSON.parse(fs.readFileSync(cocSettings))
+        : {}
+
     return {
         ...settings,
         languageServer: {
@@ -38,11 +41,16 @@ const includeLanguageServer = (cocSettings, binPath, languageServer) => {
             [languageServer]: whichLang(binPath, languageServer) 
         }
     }
-};
+}
 
+const [,, cocSettings, binPath, languageServer] = process.argv
+console.log(
+    `
+    coc settings: ${cocSettings}
+    ${languageServer} language server binary path: ${binPath}
+    `
+)
 
-const [,, cocSettings, binPath, languageServer] = process.argv;
-// fs.writeFileSync(
 cocSettings && fs.writeFileSync(
     cocSettings,
     JSON.stringify(
