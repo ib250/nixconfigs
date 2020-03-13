815cf357f68de55cab40fe23aabf0e1577583d65
Ismail Bello <ismail.bello@cmrsurgical.com>
2020-03-13 00:10:53 +0000

    latest osx

---
+++ dots/nix-shells/pyenv.nix
@@ -0,0 +1,26 @@
+{ python-version }:
+
+let
+    pkgs   = import <nixpkgs> {};
+    from   = where: what: builtins.getAttr what where;
+    python = from pkgs "python${python-version}";
+    pypi   = from pkgs "python${python-version}Packages";
+
+    stdPythonEnv = (_: with pypi; [
+        pip jedi flake8 pynvim virtualenv setuptools
+        python-language-server.override { pylint = null; }
+    ]);
+    
+
+in pkgs.mkShell {
+
+    buildInputs = [
+        pypi.tox
+        pypi.virtualenv
+        (python.withPackages stdPythonEnv)
+    ];
+
+    shellHook = ''
+    '';
+
+}
