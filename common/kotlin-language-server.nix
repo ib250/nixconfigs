1d9a93f9336c44064251c6acaca7b044d2fe79b5
ib250 <ismail.bello.513@gmail.com>
2020-10-25 19:28:46 +0000

    kotlin-language-server derivation

---
+++ common/kotlin-language-server.nix
@@ -0,0 +1,33 @@
+
+{
+  pkgs ? import <nixpkgs> {}
+  , jdk ? pkgs.jdk11
+}:
+let
+
+  owner = "fwcd";
+  repo = "kotlin-language-server";
+  version = "0.7.0";
+  sha256 = "0by07h2ly84dzmwzjf3fsgghm3fwhyhhbnnv3kl7dy1iajhl4shj";
+
+in pkgs.stdenv.mkDerivation {
+
+  name = repo;
+  inherit version;
+  src = pkgs.fetchFromGitHub {
+    rev = version;
+    inherit owner repo sha256;
+  };
+
+  propagatedBuildInputs = [ jdk ];
+  buildPhase = ''
+    ./gradlew :server:installDist
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    mv server/build/install/server/bin $out/.
+    mv server/build/install/server/lib $out/.
+  '';
+
+}
