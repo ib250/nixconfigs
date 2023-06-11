47c0ba8720e981c498e4455c21995d8d523c0a66
Ismail Bello <ismail.bello.513@gmail.com>
2023-06-11 18:49:49 +0100

    sync

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url =
-      "github:nixos/nixpkgs/nixpkgs-unstable";
+      "github:nixos/nixpkgs/nixpkgs-23.05-darwin";
 
     home-manager = {
       url = "github:nix-community/home-manager";
