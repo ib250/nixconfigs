e05abdfcf11782f2bc7baaa404c5ea383fbb3161
Ismail Bello <ismail.bello.513@gmail.com>
2023-06-21 11:39:10 +0100

    sync

--- home.nix
+++ home.nix
@@ -62,8 +62,8 @@ in
     enable = true;
     globals.mapleader = ";";
     options = {
-        relativenumber = true;
-        foldminlines = 2;
+      relativenumber = true;
+      foldminlines = 2;
     };
 
     colorschemes.nord.enable = true;
@@ -120,14 +120,60 @@ in
     plugins.lsp-format.enable = true;
     plugins.lsp-lines.enable = true;
 
-    plugins.telescope.enable=true;
+    plugins.telescope.enable = true;
     plugins.telescope.extensions.fzf-native.enable = true;
     plugins.rust-tools.enable = true;
 
     extraPlugins = [
-        pkgs.vimPlugins.nvim-lsputils
-        pkgs.vimPlugins.popfix
+      pkgs.vimPlugins.nvim-lsputils
+      pkgs.vimPlugins.popfix
     ];
+
+    extraConfigLuaPost = ''
+          if vim.fn.has('nvim-0.5.1') == 1 then
+          vim.lsp.handlers['textDocument/codeAction'] = require'lsputil.codeAction'.code_action_handler
+          vim.lsp.handlers['textDocument/references'] = require'lsputil.locations'.references_handler
+          vim.lsp.handlers['textDocument/definition'] = require'lsputil.locations'.definition_handler
+          vim.lsp.handlers['textDocument/declaration'] = require'lsputil.locations'.declaration_handler
+          vim.lsp.handlers['textDocument/typeDefinition'] = require'lsputil.locations'.typeDefinition_handler
+          vim.lsp.handlers['textDocument/implementation'] = require'lsputil.locations'.implementation_handler
+          vim.lsp.handlers['textDocument/documentSymbol'] = require'lsputil.symbols'.document_handler
+          vim.lsp.handlers['workspace/symbol'] = require'lsputil.symbols'.workspace_handler
+      else
+          local bufnr = vim.api.nvim_buf_get_number(0)
+
+          vim.lsp.handlers['textDocument/codeAction'] = function(_, _, actions)
+              require('lsputil.codeAction').code_action_handler(nil, actions, nil, nil, nil)
+          end
+
+          vim.lsp.handlers['textDocument/references'] = function(_, _, result)
+              require('lsputil.locations').references_handler(nil, result, { bufnr = bufnr }, nil)
+          end
+
+          vim.lsp.handlers['textDocument/definition'] = function(_, method, result)
+              require('lsputil.locations').definition_handler(nil, result, { bufnr = bufnr, method = method }, nil)
+          end
+
+          vim.lsp.handlers['textDocument/declaration'] = function(_, method, result)
+              require('lsputil.locations').declaration_handler(nil, result, { bufnr = bufnr, method = method }, nil)
+          end
+
+          vim.lsp.handlers['textDocument/typeDefinition'] = function(_, method, result)
+              require('lsputil.locations').typeDefinition_handler(nil, result, { bufnr = bufnr, method = method }, nil)
+          end
+
+          vim.lsp.handlers['textDocument/implementation'] = function(_, method, result)
+              require('lsputil.locations').implementation_handler(nil, result, { bufnr = bufnr, method = method }, nil)
+          end
+
+          vim.lsp.handlers['textDocument/documentSymbol'] = function(_, _, result, _, bufn)
+              require('lsputil.symbols').document_handler(nil, result, { bufnr = bufn }, nil)
+          end
+
+          vim.lsp.handlers['textDocument/symbol'] = function(_, _, result, _, bufn)
+              require('lsputil.symbols').workspace_handler(nil, result, { bufnr = bufn }, nil)
+          end
+    '';
 
     extraPackages = with builtins;
       concatLists [
